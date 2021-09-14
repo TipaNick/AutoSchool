a20ececd1da52af3348c2897d767b32eb00cec90
Nick <visualjaw3@gmail.com>
2021-09-14 18:23:28 +0700

    Add struct

--- Project1/MyForm.cpp
+++ Project1/MyForm.cpp
@@ -12,3 +12,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Application::Run(gcnew MyForm);
     return 0;
 }
+
+struct kursant {
+    wchar_t fio[150];
+    int age;
+    wchar_t email[50];
+    wchar_t phone[50];
+    wchar_t category[50];
+};
+
+struct instructor {
+    wchar_t fio[150];
+    int age;
+    wchar_t email[50];
+    wchar_t phone[50];
+    int exp;
+    wchar_t car[50];
+};
+
+struct car
+{
+    wchar_t model[50];
+    wchar_t num[50];
+};
+
+struct record {
+    wchar_t fio_k[150];
+    wchar_t fio_p[150];
+    wchar_t type[50];
+    wchar_t time[50];
+    wchar_t car_num[50];
+};
+
+struct GIBDD {
+    wchar_t fio_s[150];
+    wchar_t time[50];
+};
