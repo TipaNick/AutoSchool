97c66986f2b8a0228ea51a8a9ab5a30db455a330
Nick <visualjaw3@gmail.com>
2021-09-14 18:11:49 +0700

    Добавьте файлы проекта.

---
+++ Project1/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+#include <Windows.h>
+#include <fstream>
+#include <string>
+
+using namespace Project1; // Название проекта
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MyForm);
+    return 0;
+}
